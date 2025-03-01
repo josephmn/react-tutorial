2b2a15b4cc461c3d866dbb57d719a3afb83d12b6
Joseph Magallanes <josephcarlos.jcmn@gmail.com>
2025-02-28 22:51:30 -0500

    agregando hooks useState

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,20 @@ import Title from './components/Title'
 import Button from './components/Button'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  console.log(count);
 
   return (
     <>
-    <Title/>
+    {/* <Title/>
     <Title text="texto del componente 2"/>
     <Title text="texto del componente 3"/>
 
     <Button/>
     <Button onClick={() => alert("Primer boton")}/>
-    <Button onClick={() => alert("Segundo boton")} text = "See more"/>
+    <Button onClick={() => alert("Segundo boton")} text = "See more"/> */}
+    <Button onClick={() => setCount(count+1)} text = "+1"/>
+      <Title text={`El numero es: ${count}`}/>
     </>
   )
 }
